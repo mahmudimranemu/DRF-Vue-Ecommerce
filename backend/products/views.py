5ce202f12e7e8f7f0c12ec8e423cad9937621452
mahmudimranemu <mahmudimranemu@gmail.com>
2024-02-22 19:45:20 +0600

    Latest product APIView created

---
+++ backend/products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializers
+
+
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[0:4]
+        serializer = ProductSerializers(products, many=True)
+        return Response(serializer.data)
